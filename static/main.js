e9a55bdba4331d7431feb0505af129987f02c4f8
barnabashub <66264092+barnabashub@users.noreply.github.com>
2023-12-21 15:11:27 +0000

    show basic data

--- static/main.js
+++ static/main.js
@@ -19,6 +19,28 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+function displayPushupsTable(pushups) {
+    const tableBody = document.getElementById('pushupsTableBody');
+
+    // Clear existing rows
+    tableBody.innerHTML = '';
+
+    // Append new rows for each pushup record
+    pushups.forEach(pushup => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${pushup.id}</td>
+            <td>${pushup.userid}</td>
+            <td>${pushup.date}</td>
+            <td>${pushup.amount}</td>
+            <td>
+                <a href="#" class="btn btn-sm btn-primary">Edit</a>
+            </td>
+        `;
+        tableBody.appendChild(row);
+    });
+}
+
 function fetchData() {
     // Example: Fetch data from the backend
     fetch('https://barnabs.pythonanywhere.com/api/pushups')  // Adjust the URL based on your Flask routes
